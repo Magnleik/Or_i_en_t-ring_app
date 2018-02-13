387944c93ad42dbb74ae4315264667a99a886bff
Magnleik <magnleik@stud.ntnu.no>
2018-02-13 15:14:11 +0100

    Added Startup screen
    Set the screen as default for the app

---
+++ TRing/app/src/main/java/no/teacherspet/tring/StartupMenu.java
@@ -0,0 +1,28 @@
+package no.teacherspet.tring;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+/**
+ * Created by magnus on 13.02.2018.
+ */
+
+public class StartupMenu extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startupmenu);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    public void orientering(View v){
+        Intent intent = new Intent(StartupMenu.this,MapsActivity.class);
+        startActivity(intent);
+    }
+}
