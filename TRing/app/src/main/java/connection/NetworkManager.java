54a72883f38576402f7e141559e3f41b29de05b4
EirikOsnes <eirik.osnes@gmail.com>
2018-02-15 14:42:51 +0100

    Added some functionality to the NetworkManager

--- TRing/app/src/main/java/connection/NetworkManager.java
+++ TRing/app/src/main/java/connection/NetworkManager.java
@@ -1,5 +1,7 @@
 package connection;
 
+import java.util.List;
+
 /**
  * Created by Eirik on 15-Feb-18.
  */
@@ -20,9 +22,40 @@ public class NetworkManager {
         return nm;
     }
 
-    public void addPoint(IPoints point){
+    /**
+     * Adds a point to the database, not affiliated with an Event
+     * @param point The Point to be added
+     */
+    public void addPoint(Point point){
 
     }
 
+    /**
+     * Finds all points within a circle with radius maxDist from the given location
+     * @param latitude
+     * @param longitude
+     * @param maxDist
+     * @return
+     */
+    public List<Point> getPointsNearby(double latitude, double longitude, double maxDist){
+        return null;
+    }
 
+    /**
+     * Finds all points associated with an Event. First element will always be the starting Point
+     * @param event
+     * @return
+     */
+    public List<Point> getPointsFromEvent(Event event){
+        return null;
+    }
+
+    /**
+     * Updates the existing event if it exists in the database, will otherwise create it and return the event with its updated information.
+     * @param event
+     * @return
+     */
+    public Event updateEvent(Event event){
+        return null;
+    }
 }
