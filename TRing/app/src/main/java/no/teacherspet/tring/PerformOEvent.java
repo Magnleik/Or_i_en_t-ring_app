367609d1caf76c8db56413af5b2232815fce6fdd
pettersb <pettersb@stud.ntnu.no>
2018-02-15 16:59:05 +0100

    Added funcionality; add points, save event, show position

--- TRing/app/src/main/java/no/teacherspet/tring/PerformOEvent.java
+++ TRing/app/src/main/java/no/teacherspet/tring/PerformOEvent.java
@@ -1,13 +1,111 @@
 package no.teacherspet.tring;
 
+import android.content.pm.PackageManager;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class PerformOEvent extends AppCompatActivity {
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class PerformOEvent extends FragmentActivity implements GoogleMap.OnMyLocationButtonClickListener, OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private int countDown = 5;
+    private int secondsLeft;
+    private Timer timer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perform_oevent);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map_used_in_event);
+        mapFragment.getMapAsync(this);
+        //Log.i("INFO:", getString(R.string.google_maps_key));
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in Sydney and move the camera
+        LatLng gløs = new LatLng(63.416136, 10.405297);
+        mMap.addMarker(new MarkerOptions().position(gløs).title("Gløs:))"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(gløs));
+    }
+
+    public void showPosition(View v) {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+            mMap.setOnMyLocationButtonClickListener(this);
+            this.reset();
+        }
+    }
+
+    public void hidePosition(View v) {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(false);
+            mMap.setOnMyLocationButtonClickListener(this);
+        }
+    }
+
+
+    public void reset() {
+        timer = new Timer();
+        secondsLeft = countDown;
+        // Decrease seconds left every 1 second.
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                secondsLeft--;
+                if (secondsLeft == 0) {
+                    timer.cancel();
+                }
+            }
+        }, 0, 1000);
+        //Kall hidePosition
+    }
+
+
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+
 }
+
