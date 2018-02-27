a7e9d87d9a5e5ba69720626a6e71b25d9ae60d2f
Magnleik <magnleik@stud.ntnu.no>
2018-02-27 14:31:45 +0100

    able to change activity from popUpActivity

--- TRing/app/src/main/java/no/teacherspet/tring/PopupPointDesc.java
+++ TRing/app/src/main/java/no/teacherspet/tring/PopupPointDesc.java
@@ -1,15 +1,21 @@
 package no.teacherspet.tring;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 public class PopupPointDesc extends Activity {
 
+    public boolean savePoint;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        savePoint=false;
         setContentView(R.layout.activity_popup_point_desc);
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -19,5 +25,15 @@ public class PopupPointDesc extends Activity {
         EditText name = (EditText) findViewById(R.id.set_point_name);
         name.setText("Punkt");
         name.selectAll();
+        InputMethodManager imm= (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(name,InputMethodManager.SHOW_IMPLICIT);
+    }
+
+    public void onOkClick(View v){
+        savePoint = true;
+        finish();
+    }
+    public void onCancelClick(View v){
+        finish();
     }
 }
