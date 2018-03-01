00e22efd5adfad7d938001812d99fe61b71c5423
Magnleik <magnleik@stud.ntnu.no>
2018-03-01 17:12:22 +0100

    added list over created events for testing

--- TRing/app/src/main/java/no/teacherspet/tring/CreateOEvent.java
+++ TRing/app/src/main/java/no/teacherspet/tring/CreateOEvent.java
@@ -162,6 +162,7 @@ public class CreateOEvent extends AppCompatActivity implements OnMapReadyCallbac
         for(Marker marker:arrayListWithCoords){
             event.addPost(new Point(marker.getPosition().latitude,marker.getPosition().longitude,marker.getTitle()));
         }
+        StartupMenu.testEvents.add(event);
         Toast.makeText(getApplicationContext(), "Lagret ruten '" + eventTitle + "', " + arrayListWithCoords.size() + " punkt registrert", Toast.LENGTH_LONG).show();
         //LAGRE
         //Reset
