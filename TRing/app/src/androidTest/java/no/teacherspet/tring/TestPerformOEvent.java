9ad01cae46ffda5d3e4bbd24128edb2bbc18d18c
Magnleik <magnleik@stud.ntnu.no>
2018-03-06 15:24:07 +0100

    added mockito to project

--- TRing/app/src/androidTest/java/no/teacherspet/tring/TestPerformOEvent.java
+++ TRing/app/src/androidTest/java/no/teacherspet/tring/TestPerformOEvent.java
@@ -3,14 +3,16 @@ package no.teacherspet.tring;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
 /**
  * Created by petterbjorkaas on 22/02/2018.
  */
-
+@RunWith(MockitoJUnitRunner.class)
 public class TestPerformOEvent {
     @Rule
     public final ActivityTestRule<PerformOEvent> main = new ActivityTestRule<PerformOEvent>(PerformOEvent.class);
-
+    
 
 }
