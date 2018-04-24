224317c7d74da9cd5bb2800c8338120ead55ab61
Magnleik <magnleik@stud.ntnu.no>
2018-04-24 12:31:51 +0200

    markers change color based on selected, unselected and added. Now able to select points in nearby points

--- TRing/app/src/main/java/no/teacherspet/tring/activities/AddExistingPoint.java
+++ TRing/app/src/main/java/no/teacherspet/tring/activities/AddExistingPoint.java
@@ -1,19 +1,25 @@
 package no.teacherspet.tring.activities;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 
@@ -33,11 +39,14 @@ public class AddExistingPoint extends AppCompatActivity implements OnMapReadyCal
     GoogleMap mMap;
     FusedLocationProviderClient lm;
     NetworkManager networkManager;
+    ArrayList<LatLng> selectedPoints;
+    Marker selectedMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         networkManager = NetworkManager.getInstance();
+        selectedPoints = new ArrayList<>();
         setContentView(R.layout.activity_add_existing_point);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
@@ -52,6 +61,13 @@ public class AddExistingPoint extends AppCompatActivity implements OnMapReadyCal
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        setResult(RESULT_CANCELED,null);
+        finish();
+        return false;
+    }
+
+    @Override
     public void onMapReady(GoogleMap googleMap) {
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(getApplicationContext(), "App needs permission to access location services on phone to run", Toast.LENGTH_LONG).show();
@@ -60,19 +76,33 @@ public class AddExistingPoint extends AppCompatActivity implements OnMapReadyCal
             lm.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
                 @Override
                 public void onSuccess(Location location) {
-                    if(location != null){
+                    if (location != null) {
                         networkManager.getNearbyPoints(location.getLatitude(), location.getLongitude(), 200, new ICallbackAdapter<ArrayList<Point>>() {
                             @Override
                             public void onResponse(ArrayList<Point> object) {
-                                for(Point point:object){
-                                    mMap.addMarker(new MarkerOptions().position(new LatLng(point.getLatitude(),point.getLongitude())));
-                                    Toast.makeText(getApplicationContext(),"Select points you wish to add",Toast.LENGTH_SHORT).show();
+                                for (Point point : object) {
+                                    mMap.addMarker(new MarkerOptions().position(new LatLng(point.getLatitude(), point.getLongitude())));
                                 }
+                                Toast.makeText(getApplicationContext(), "Select points you wish to add", Toast.LENGTH_SHORT).show();
+                                mMap.moveCamera(CameraUpdateFactory.zoomTo(20.0f));
+                                mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+                                mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                                    @Override
+                                    public boolean onMarkerClick(Marker marker) {
+                                        if(selectedMarker != null){
+                                            selectedMarker.setIcon(BitmapDescriptorFactory.defaultMarker());
+                                        }
+                                        selectedMarker = marker;
+                                        selectedMarker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+                                        selectedMarker.showInfoWindow();
+                                        return true;
+                                    }
+                                });
                             }
 
                             @Override
                             public void onFailure(Throwable t) {
-                                Toast.makeText(getApplicationContext(),"Something went wrong",Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
                             }
                         });
                     }
@@ -80,4 +110,20 @@ public class AddExistingPoint extends AppCompatActivity implements OnMapReadyCal
             });
         }
     }
+
+    public void addButtonClick(View v){
+        if(selectedMarker != null){
+            selectedPoints.add(selectedMarker.getPosition());
+            selectedMarker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+        }
+    }
+
+    public void doneAddingClick(View v){
+        Intent intent = new Intent();
+        intent.putExtra("SelectedPositions", selectedPoints);
+        setResult(RESULT_OK,intent);
+        finish();
+    }
+
+
 }
