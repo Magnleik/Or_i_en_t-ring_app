2407aaf033ab0e59ebdca11a3bc8c46c350dffd8
EirikOsnes <eirik.osnes@gmail.com>
2018-04-24 14:10:54 +0200

    Log in functional

--- TRing/app/src/main/java/no/teacherspet/tring/activities/StartupMenu.java
+++ TRing/app/src/main/java/no/teacherspet/tring/activities/StartupMenu.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import connection.Event;
+import connection.NetworkManager;
 import io.reactivex.disposables.Disposable;
 import no.teacherspet.tring.Database.Entities.RoomUser;
 import no.teacherspet.tring.Database.LocalDatabase;
@@ -67,6 +68,15 @@ public class StartupMenu extends AppCompatActivity{
     public void createUserButton(View v){
         Intent intent = new Intent(StartupMenu.this,CreateUserActivity.class);
         startActivity(intent);
+    }
+
+    public void logInButton(View v){
+        Intent intent = new Intent(StartupMenu.this,LogInActivity.class);
+        startActivity(intent);
+    }
+
+    public void logOutButton(View v){
+        NetworkManager.getInstance().logOut();
     }
 
     private boolean requestAccess(){
