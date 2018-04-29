aee8309eaab222a9e8e07ae1e2acb6311d7b9f1a
pettersb <pettersb@stud.ntnu.no>
2018-04-29 18:10:56 +0200

    Cleaningx2

--- TRing/app/src/main/java/no/teacherspet/tring/fragments/MyEvents.java
+++ TRing/app/src/main/java/no/teacherspet/tring/fragments/MyEvents.java
@@ -46,14 +46,7 @@ import no.teacherspet.tring.util.RoomInteract;
  * create an instance of this fragment.
  */
 public class MyEvents extends Fragment implements RoomInteract {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private ListView mListView;
     private boolean changeEvent;
     private HashMap<Integer, Event> theEventReceived;
@@ -75,16 +68,12 @@ public class MyEvents extends Fragment implements RoomInteract {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     *
      * @return A new instance of fragment MyEvents.
      */
-    // TODO: Rename and change types and number of parameters
-    public static MyEvents newInstance(String param1, String param2) {
+    public static MyEvents newInstance() {
         MyEvents fragment = new MyEvents();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -96,10 +85,6 @@ public class MyEvents extends Fragment implements RoomInteract {
         roomSaveAndLoad = new RoomSaveAndLoad(getContext(), this);
         changeEvent = true;
         HashMap<Integer, Event> theEventReceived = new HashMap<>();
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
